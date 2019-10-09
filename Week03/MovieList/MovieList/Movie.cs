7eafae15bd65372dd9df8e922248dab1ba317e3e
Pameet <pssg04@gmail.com>
2019-10-09 19:27:15 -0400

    Changes for MovieList

---
+++ Week03/MovieList/MovieList/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieList
+{
+    public class Movie
+    {
+        public string Title;
+        public int ReleaseYear;
+        
+        public Movie(string title, int releaseYear)
+        {
+            Title = title;
+            ReleaseYear = releaseYear;
+        }
+
+        public void MovieInfo()
+        {
+            MessageBox.Show(Title + " was released in " + ReleaseYear + ".");
+        }
+    }
+}
